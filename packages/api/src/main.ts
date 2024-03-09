59934ca0bf0b9994165890accaa83f129318cca3
Matheus Lopes <inolopesm@gmail.com>
2024-03-09 09:40:59 -0300

    update client api route

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -12,7 +12,7 @@ interface Client {
   updated_at: Date;
 }
 
-interface AddClientDTO {
+interface AddOrUpdateClientDTO {
   name: string;
   email: string;
   phone: string;
@@ -44,15 +44,67 @@ server.post('/api/clients', {
         email: { type: 'string', minLength: 1, maxLength: 255, format: 'email' },
         phone: { type: 'string', minLength: 1, maxLength: 255 },
       },
-    } satisfies JSONSchemaType<AddClientDTO>,
+    } satisfies JSONSchemaType<AddOrUpdateClientDTO>,
   },
   handler: async (request, reply) => {
-    const { name, email, phone } = request.body as AddClientDTO;
+    const { name, email, phone } = request.body as AddOrUpdateClientDTO;
     const sql = 'INSERT INTO clients (name, email, phone) VALUES ($1, $2, $3) RETURNING *';
     const values = [name, email, phone];
     const query = await database.query(sql, values);
     const client = query.rows[0] as Client;
     reply.code(201);
+    reply.send(client);
+  },
+});
+
+server.put<{ Params: { id: string } }>('/api/clients/:id', {
+  schema: {
+    params: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['id'],
+      properties: { id: { type: 'string', format: 'uuid' } },
+    },
+    body: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['name', 'email', 'phone'],
+      properties: {
+        name: { type: 'string', minLength: 1, maxLength: 255 },
+        email: { type: 'string', minLength: 1, maxLength: 255, format: 'email' },
+        phone: { type: 'string', minLength: 1, maxLength: 255 },
+      },
+    } satisfies JSONSchemaType<AddOrUpdateClientDTO>,
+  },
+  handler: async (request, reply) => {
+    const { id } = request.params;
+
+    let getClientQuery: pg.QueryResult<Client>;
+
+    {
+      const sql = 'SELECT * FROM clients WHERE id = $1';
+      const values = [id];
+      getClientQuery = await database.query<Client>(sql, values);
+    }
+
+    if (getClientQuery.rowCount === 0) {
+      reply.code(404);
+      reply.send({ message: 'client not found' });
+      return;
+    }
+
+    const { name, email, phone } = request.body as AddOrUpdateClientDTO;
+
+    let updateClientQuery: pg.QueryResult<Client>;
+
+    {
+      const sql = 'UPDATE clients SET name = $1, email = $2, phone = $3 WHERE id = $4 RETURNING *';
+      const values = [name, email, phone, id];
+      updateClientQuery = await database.query(sql, values);
+    }
+
+    const client = updateClientQuery.rows[0] as Client;
+    reply.code(200);
     reply.send(client);
   },
 });
